140534783da9d3059582be63d69aaee127e4210a
Jahid01234 <mdjahidhasan790523@gmail.com>
2024-10-28 17:20:57 +0600

    - initially project setup - Firebase setup

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
